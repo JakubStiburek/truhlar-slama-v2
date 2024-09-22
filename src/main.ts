ac0537782812237d68d7829501ec97fe87cea440
Jakub Stiburek <jstiburek@splashsports.com>
2024-09-22 20:16:45 +0200

    refactor head partial

--- src/main.ts
+++ src/main.ts
@@ -48,6 +48,23 @@ async function bootstrap() {
       '  </nav>\n' +
       '</header>',
   );
+  hbs.registerPartial(
+    'head',
+    '  <meta charset="utf-8" />\n' +
+      '  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />\n' +
+      '  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />\n' +
+      '  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />\n' +
+      '  <link rel="manifest" href="/site.webmanifest" />\n' +
+      '  <meta name="viewport" content="width=device-width" />\n' +
+      '  <meta name="description"\n' +
+      '        content="Truhlář Ondřej Sláma. Řemeslná výroba ze dřeva - nábytek, zakázková výroba, unikátní kousky. Tel: 608334824, mail: ondrej.slama@truhlarslama.cz" />\n' +
+      '  <link rel="preconnect" href="https://fonts.googleapis.com">\n' +
+      '  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>\n' +
+      '  <link\n' +
+      '    href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,400;0,700;1,400&display=swap"\n' +
+      '    rel="stylesheet">\n' +
+      '  <link rel="stylesheet" type="text/css" href="css/common.css" />',
+  );
 
   await app.listen(3000);
 }
